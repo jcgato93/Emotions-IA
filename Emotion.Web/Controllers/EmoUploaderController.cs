d891acf4ce8baa4df0d7d80c575c42234edcf9f9
jcgato93 <npgcamilo@gmail.com>
2018-08-25 13:57:59 -0500

    Refactorizacion por actualizacion de la API

---
+++ Emotion.Web/Controllers/EmoUploaderController.cs
@@ -0,0 +1,69 @@
+﻿using Emotion.Web.Models;
+using Emotion.Web.Util;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Emotion.Web.Controllers
+{
+    public class EmoUploaderController : Controller
+    {
+        string serverFolderPath;
+        EmotionHelper emoHelper;
+        string key;
+        EmotionWebContext db = new EmotionWebContext();
+
+        public EmoUploaderController()
+        {
+            
+            serverFolderPath = ConfigurationManager.AppSettings["UPLOAD_DIR"];
+            key = ConfigurationManager.AppSettings["EMOTION_API_KEY"];
+            emoHelper = new EmotionHelper(key);
+        }
+
+        // GET: EmoUploader
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> IndexAsync(HttpPostedFileBase file)
+        {
+            if (file?.ContentLength>0)
+            {
+                var pictureName = Guid.NewGuid().ToString();
+                pictureName += Path.GetExtension(file.FileName);
+
+                var route = Server.MapPath(serverFolderPath);//optiene la ruta absoluta de la carpeta
+                if (!Directory.Exists(route))
+                {
+                    Directory.CreateDirectory(route);
+                }
+                route = route + "/" + pictureName;
+
+                
+                file.SaveAs(@route);
+
+              var emoPicture= await  emoHelper.DetectAndExtractFacesAsync(file.InputStream);
+
+                emoPicture.Name = file.FileName;
+                emoPicture.Path = $"{serverFolderPath}/{pictureName}";
+                using (EmotionWebContext database = new EmotionWebContext())
+                {
+                    db.EmoPictures.Add(emoPicture);
+                    await db.SaveChangesAsync();
+                }
+                    
+
+                return RedirectToAction("Details", "EmoPictures", new { Id = emoPicture.Id });
+            }
+            return View();
+        }
+    }
+}
--- Emotion.Web/Util/EmotionHelper.cs
+++ Emotion.Web/Util/EmotionHelper.cs
@@ -1,90 +1,85 @@
 ﻿using Emotion.Web.Models;
 using Microsoft.ProjectOxford.Common.Contract;
 using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Face.Contract;
+using Microsoft.ProjectOxford.Face;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Web;
+using System.Collections.ObjectModel;
 using System.Reflection;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace Emotion.Web.Util
 {
     public class EmotionHelper
     {
-        public EmotionServiceClient emoClient;
+        public FaceServiceClient emoClient;
 
 
         public EmotionHelper(string key)
         {
-            emoClient = new EmotionServiceClient(key);
+            emoClient = new FaceServiceClient(key, "https://westcentralus.api.cognitive.microsoft.com/face/v1.0");
         }
 
 
-        public async Task<EmoPicture> DetectAndExtracFacesAsync(Stream imageStream)
+        public async Task<EmoPicture> DetectAndExtractFacesAsync(Stream imageStream)
         {
-           Microsoft.ProjectOxford.Common.Contract.Emotion[] emotions= await emoClient.RecognizeAsync(imageStream);
+            IEnumerable<FaceAttributeType> faceAttributes = new FaceAttributeType[] { FaceAttributeType.Emotion };
+            Face[] faces = await emoClient.DetectAsync(imageStream, false, false, faceAttributes);
 
             var emoPicture = new EmoPicture();
 
-            emoPicture.Faces = ExtacFaces(emotions,emoPicture);
+            emoPicture.Faces = ExtractFaces(faces, emoPicture);
 
             return emoPicture;
+
         }
 
-        private ObservableCollection<EmoFace> ExtacFaces(Microsoft.ProjectOxford.Common.Contract.Emotion[] emotions,
-            EmoPicture emoPicture)
+        private ObservableCollection<EmoFace> ExtractFaces(Face[] faces, EmoPicture emoPicture)
         {
             var listaFaces = new ObservableCollection<EmoFace>();
 
-            foreach (var emotion in emotions)
+            foreach (var face in faces)
             {
-                var emoface = new EmoFace();
-                emoPicture.Faces.Add(new EmoFace()
+                var emoFace = new EmoFace()
                 {
-                     X=emotion.FaceRectangle.Left,
-                     Y=emotion.FaceRectangle.Top,
-                     Width=emotion.FaceRectangle.Width,
-                     Height= emotion.FaceRectangle.Height,
-                     Picture=emoPicture
+                    X = face.FaceRectangle.Left,
+                    Y = face.FaceRectangle.Top,
+                    Width = face.FaceRectangle.Width,
+                    Height = face.FaceRectangle.Height,
+                    Picture = emoPicture
 
-                });
-                emoface.Emotions = ProcessEmotions(emotion.Scores,emoface);
-                listaFaces.Add(emoface);
+                };
+                emoFace.Emotions = ProcessEmotions(face.FaceAttributes.Emotion, emoFace);
+                listaFaces.Add(emoFace);
 
             }
+
             return listaFaces;
         }
 
-        private ObservableCollection<EmoEmotion> ProcessEmotions(EmotionScores scores,
-            EmoFace emoFace)
+        private ObservableCollection<EmoEmotion> ProcessEmotions(EmotionScores scores, EmoFace emoFace)
         {
             var emotionList = new ObservableCollection<EmoEmotion>();
+            var properties = scores.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var filterproperties = properties.Where(p => p.PropertyType == typeof(float));
 
-            //Extraer las propiedades de los Scores
-            //BindingFlags especifica el tipo de propiedades que extraera
-            var properties = scores.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance );
-
-            //optine todas las propiedades de tipo float
-            var filterProperties = properties.Where(p => p.PropertyType == typeof(float));
-
-            var emotype = EmoEmotionEnum.Undetermined;
-            foreach (var prop in filterProperties)
+            var emoType = EmoEmotionEnum.Undetermined;
+            foreach (var prop in filterproperties)
             {
-                //intenta parsear la propiedad que viene del servicio en el tipo enum
-                if(!Enum.TryParse<EmoEmotionEnum>(prop.Name,out emotype)){//prop.Name nombre de la propiedad, donde se guardara el resultado
-                    emotype = EmoEmotionEnum.Undetermined;
-                }
-
-
+                if (!Enum.TryParse<EmoEmotionEnum>(prop.Name, out emoType))
+                    emoType = EmoEmotionEnum.Undetermined;
                 var emoEmotion = new EmoEmotion();
-                emoEmotion.Score =(float) prop.GetValue(scores);
-                emoEmotion.EmotionType = emotype;
+                emoEmotion.Score = (float)prop.GetValue(scores);
+                emoEmotion.EmotionType = emoType;
+                emoEmotion.EmoFaceId = emoFace.Id;
                 emoEmotion.Face = emoFace;
 
                 emotionList.Add(emoEmotion);
+
             }
 
             return emotionList;
